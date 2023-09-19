f89c0ed48edd3cd268b5e1183ccf06542e0da2fa
NaviTheCoderboi <102732282+NaviTheCoderboi@users.noreply.github.com>
2023-09-19 12:04:05 +0530

    added heo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 
-import svelte from "@astrojs/svelte";
-
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), partytown(), svelte()],
+    integrations: [tailwind(), partytown()],
 });
