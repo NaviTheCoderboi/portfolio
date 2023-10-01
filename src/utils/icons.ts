1162a1fd1dff3c0cc7f555e48c0f28cc798a2a6f
NaviTheCoderboi <102732282+NaviTheCoderboi@users.noreply.github.com>
2023-10-01 15:21:55 +0530

    improved performance

---
+++ src/utils/icons.ts
@@ -0,0 +1,31 @@
+import css from "+asset@icons/css.svg?url";
+import docker from "+asset@icons/docker.svg?url";
+import git from "+asset@icons/git.svg?url";
+import html from "+asset@icons/html.svg?url";
+import javascript from "+asset@icons/javascript.svg?url";
+import mongodb from "+asset@icons/mongodb.svg?url";
+import nextjs from "+asset@icons/nextjs.svg?url";
+import pg from "+asset@icons/pg.svg?url";
+import python from "+asset@icons/python.svg?url";
+import react from "+asset@icons/react.svg?url";
+import rust from "+asset@icons/rust.png?url";
+import svelte from "+asset@icons/svelte.svg?url";
+import typescript from "+asset@icons/typescript.svg?url";
+import ubuntu from "+asset@icons/ubuntu.svg?url";
+
+export default [
+    css,
+    docker,
+    git,
+    html,
+    javascript,
+    mongodb,
+    nextjs,
+    pg,
+    python,
+    react,
+    rust,
+    svelte,
+    typescript,
+    ubuntu,
+];
